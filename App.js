463b82583efc563792065210a040be2c1e5a57bc
Max Cook <maxwell.h.cook@gmail.com>
2023-08-05 15:57:31 -0700

    FlatList exercise

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import ColorBox from './components/ColorBox';
 
-export default function App() {
+const App = () => {
+  const COLORS = [
+    { colorName: 'Base03', hexCode: '#002b36' },
+    { colorName: 'Base02', hexCode: '#073642' },
+    { colorName: 'Base01', hexCode: '#586e75' },
+    { colorName: 'Base00', hexCode: '#657b83' },
+    { colorName: 'Base0', hexCode: '#839496' },
+    { colorName: 'Base1', hexCode: '#93a1a1' },
+    { colorName: 'Base2', hexCode: '#eee8d5' },
+    { colorName: 'Base3', hexCode: '#fdf6e3' },
+    { colorName: 'Yellow', hexCode: '#b58900' },
+    { colorName: 'Orange', hexCode: '#cb4b16' },
+    { colorName: 'Red', hexCode: '#dc322f' },
+    { colorName: 'Magenta', hexCode: '#d33682' },
+    { colorName: 'Violet', hexCode: '#6c71c4' },
+    { colorName: 'Blue', hexCode: '#268bd2' },
+    { colorName: 'Cyan', hexCode: '#2aa198' },
+    { colorName: 'Green', hexCode: '#859900' },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <FlatList 
+            style={styles.listContainer}
+            data={COLORS}
+            keyExtractor={item => item.colorName}
+            renderItem={({item}) => (
+              <ColorBox 
+                colorName={item.colorName}
+                hexCode={item.hexCode}
+              />
+            )}
+            ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+          />
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20
   },
+  safeArea: {
+    flex: 2,
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  listContainer: {
+    width: '100%'
+  }
 });
+
+export default App;
---
+++ App_Old.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.pink]}>
+        <Text>Hello, world!!!</Text>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: 'pink',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    // backgroundColor: 'red'
+  },
+  safeArea: {
+    flex: 2,
+  }
+});
+
+export default App;
